25340b954fdd207a62a94fa4c549574cc7d5ead5
Bruno <valaeraa@gmail.com>
2019-05-07 14:45:05 +0200

    Create People Page
    Created the people page and populated
    it with data from sql server.

--- src/TrackerLibrary/Models/PersonModel.cs
+++ src/TrackerLibrary/Models/PersonModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,21 +20,33 @@ namespace TrackerLibrary.Models
         /// <summary>
         /// The first name of the person.
         /// </summary>
+        [Display(Name = "First Name")]
+        [StringLength(50, MinimumLength = 2)]
+        [Required]
         public string FirstName { get; set; }
 
         /// <summary>
         /// The last name of the person.
         /// </summary>
+        [Display(Name = "Last Name")]
+        [StringLength(50, MinimumLength = 2)]
+        [Required]
         public string LastName { get; set; }
 
         /// <summary>
         /// The primary email address of the person.
         /// </summary>
+        [Display(Name = "Email Address")]
+        [StringLength(100, MinimumLength = 6)]
+        [Required]
         public string EmailAddress { get; set; }
 
         /// <summary>
         /// The primary cell phone number of  the person.
         /// </summary>
+        [Display(Name = "Cellphone Number")]
+        [StringLength(20, MinimumLength = 6)]
+        [Required]
         public string CellphoneNumber { get; set; }
 
         /// <summary>
---
+++ src/TrackerMVCUI/Controllers/PeopleController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackerLibrary;
+using TrackerLibrary.Models;
+
+namespace TrackerMVCUI.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            List<PersonModel> availiblePeople = GlobalConfig.Connection.GetPerson_All();
+
+            return View(availiblePeople);
+        }
+
+        // GET: People/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        [HttpPost]
+        public ActionResult Create(PersonModel p)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    GlobalConfig.Connection.CreatePerson(p);
+
+                    return RedirectToAction("Index"); 
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
