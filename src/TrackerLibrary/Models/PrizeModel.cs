4cf378e267b3c41d07efcb2bc44ddad511a89477
Bruno <valaeraa@gmail.com>
2019-05-08 15:45:10 +0200

    Add Prize Page
    Added the prizes page for viewing
    and creating new prizes.

--- src/TrackerLibrary/Models/PrizeModel.cs
+++ src/TrackerLibrary/Models/PrizeModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,22 @@ namespace TrackerLibrary.Models
     public class PrizeModel
     {
         public int Id { get; set; }
+
+        [Display(Name = "Place Number")]
+        [Range(1, 2)]
+        [Required]
         public int PlaceNumber { get; set; }
+
+        [Display(Name = "Place Name")]
+        [StringLength(50, MinimumLength = 3)]
+        [Required]
         public string PlaceName { get; set; }
+
+        [Display(Name = "Prize Amount")]
+        [DataType(DataType.Currency)]
         public decimal PrizeAmount { get; set; }
+
+        [Display(Name = "Prize Percentage")]
         public double PrizePercentage { get; set; }
 
         public PrizeModel()
---
+++ src/TrackerMVCUI/Controllers/PrizesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackerLibrary;
+using TrackerLibrary.Models;
+
+namespace TrackerMVCUI.Controllers
+{
+    public class PrizesController : Controller
+    {
+        // GET: Prizes
+        public ActionResult Index()
+        {
+            List<PrizeModel> allPrizes = GlobalConfig.Connection.GetPrizes_All();
+
+            return View(allPrizes);
+        }
+
+        // GET: Prizes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Prizes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Create(PrizeModel p)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    GlobalConfig.Connection.CreatePrize(p);
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
