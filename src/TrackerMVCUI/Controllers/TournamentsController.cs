7181a3484d19f3f607d3732f43c7a86df0d3033c
Bruno <valaeraa@gmail.com>
2019-05-09 15:15:22 +0200

    Add Create Tournament Page
    Added Create Tournament page and
    a Tournament Model

---
+++ src/TrackerMVCUI/Controllers/TournamentsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackerLibrary;
+using TrackerLibrary.Models;
+
+namespace TrackerMVCUI.Controllers
+{
+    public class TournamentsController : Controller
+    {
+        // GET: Tournaments
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: Tournaments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tournaments/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Create(TournamentModel t)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    // TODO - Create the tournament
+                    //GlobalConfig.Connection.CreateTournament(t);
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ src/TrackerMVCUI/Models/TournamentMVCModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrackerMVCUI.Models
+{
+    public class TournamentMVCModel
+    {
+        [Display(Name = "Tournament Name")]
+        [StringLength(50, MinimumLength = 2)]
+        [Required]
+        public string TournamentName { get; set; }
+
+        [Display(Name = "Entry Fee")]
+        [DataType(DataType.Currency)]
+        [Required]
+        public decimal EntryFee { get; set; }
+
+        [Display(Name = "Entered Teams")]
+        public List<SelectListItem> EnteredTeams { get; set; } = new List<SelectListItem>();
+
+        public List<string> SelectedEnteredTeams { get; set; } = new List<string>();
+
+        [Display(Name = "Prizes")]
+        [StringLength(50, MinimumLength = 2)]
+        [Required]
+        public List<SelectListItem> Prizes { get; set; } = new List<SelectListItem>();
+
+        public List<string> SelectedPrizes { get; set; } = new List<string>();
+    }
+}
