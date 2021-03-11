f4084e819d2a3ffc5e15a6fa15e9e1cb2528e4b4
Valaeraa <valaeraa@gmail.com>
2021-03-11 20:59:55 +0100

    Added .Net Core MVC Project

---
+++ src/TrackerWebUI/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TrackerLibrary.Models;
+using TrackerLibrary;
+using TrackerWebUI.Models;
+
+namespace TrackerWebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<TournamentModel> tournaments = GlobalConfig.Connection.GetTournament_All();
+
+            return View(tournaments);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
