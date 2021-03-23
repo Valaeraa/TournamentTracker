81bca92ebd8f95c60e13a257e8f8ae9afa7a9ac4
Valaeraa <valaeraa@gmail.com>
2021-03-23 16:58:37 +0100

    Add dependency injection to winform and wpf projects

--- src/TrackerUI/Program.cs
+++ src/TrackerUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,14 +15,27 @@ namespace TrackerUI
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            // Can create empty string array instead
+            var host = CreateHostBuilder(args).Build();
+
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             // Initialize the database connections
             GlobalConfig.InitializeConnections(DatabaseType.TextFile);
-            Application.Run(new TournamentDashboardForm());
+            Application.Run(host.Services.GetService<TournamentDashboardForm>());
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddTransient<TournamentDashboardForm>();
+                });
         }
     }
 }
