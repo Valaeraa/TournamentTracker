e00d8e177521158df353389943459e0a8e9ec38a
Valaeraa <valaeraa@gmail.com>
2021-04-30 17:03:11 +0200

    Add Serilog to WPF project

--- src/TrackerWPFUI/Bootstrapper.cs
+++ src/TrackerWPFUI/Bootstrapper.cs
@@ -1,24 +1,57 @@
 ﻿using Caliburn.Micro;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Extensions.Logging;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Windows;
 using TrackerWPFUI.ViewModels;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace TrackerWPFUI
 {
     public class Bootstrapper : BootstrapperBase
     {
-        private SimpleContainer _container = new SimpleContainer();
+        private IHost _host;
 
         public Bootstrapper()
         {
             Initialize();
+
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+               .Enrich.FromLogContext()
+               .WriteTo.File(@"C:\Users\bruno\Documents\Github\TournamentTracker\WPFLog.txt")
+               .CreateLogger();
+
+            _host = CreateHostBuilder(new string[0]).Build();
         }
 
-        protected override void Configure()
+        public static IHostBuilder CreateHostBuilder(string[] args)
         {
-            _container.Instance(_container)
-                .PerRequest<ShellViewModel>();
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddTransient<ShellViewModel>();
+                    services.AddSingleton<IWindowManager, WindowManager>();
+                    services.AddSingleton<IEventAggregator, EventAggregator>();
+                    services.AddSingleton<ILoggerFactory>(x =>
+                    {
+                        var providerCollection = x.GetService<LoggerProviderCollection>();
+                        var factory = new SerilogLoggerFactory(null, true, providerCollection);
+
+                        foreach (var provider in x.GetServices<ILoggerProvider>())
+                        {
+                            factory.AddProvider(provider);
+                        }
+
+                        return factory;
+                    });
+                });
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
@@ -28,17 +61,17 @@ namespace TrackerWPFUI
 
         protected override object GetInstance(Type service, string key)
         {
-            return _container.GetInstance(service, key);
+            return _host.Services.GetService(service);
         }
 
         protected override IEnumerable<object> GetAllInstances(Type service)
         {
-            return _container.GetAllInstances(service);
+            return _host.Services.GetServices(service);
         }
 
         protected override void BuildUp(object instance)
         {
-            _container.BuildUp(instance);
+            
         }
     }
 }
